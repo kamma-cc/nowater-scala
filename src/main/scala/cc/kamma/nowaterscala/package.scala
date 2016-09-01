b80a8e8d0bda1680ae7e3a742b2ed5a187225f06
Guangyu Ding <boaiwinserkua@gmail.com>
2016-09-01 13:37:07 +0800

    完成

--- src/main/scala/cc/kamma/nowaterscala/package.scala
+++ src/main/scala/cc/kamma/nowaterscala/package.scala
@@ -3,6 +3,30 @@ package cc.kamma
 package object nowaterscala {
   type Definitions = Seq[(String, Seq[(String, String)])]
 
-  def expand(d: Definitions): Definitions = ???
+  def expand(d: Definitions): Definitions = {
+
+    def noMixin(d: Definitions): Boolean = {
+      d.forall {
+        case (key, value) => value.forall {
+          case (innerKey, _) => innerKey != "mixin"
+        }
+      }
+    }
+
+    if (noMixin(d))
+      d
+    else {
+      expand(d.map {
+        case (key, value) =>
+          val newValue: Seq[(String, String)] = value.flatMap {
+            case (innerKey: String, innerValue: String) if innerKey == "mixin" => d.toMap.apply(innerValue)
+            case x: (String, String) => Seq(x)
+          }
+          (key, newValue)
+      })
+    }
+
+
+  }
 
 }
--- src/test/scala/cc/kamma/nowaterscala/packageTest.scala
+++ src/test/scala/cc/kamma/nowaterscala/packageTest.scala
@@ -10,5 +10,13 @@ class packageTest extends FunSuite {
     assert(expand(defs) == Seq("User" -> Seq("name" -> "oldpig", "gender" -> "male", "Province" -> "Shanghai", "District" -> "Minhang"),
       "Address" -> Seq("Province" -> "Shanghai", "District" -> "Minhang")))
   }
+  test("testExpand2") {
+    val defs: Definitions = Seq("User" -> Seq("name" -> "oldpig", "gender" -> "male", "mixin" -> "Address"),
+      "Address" -> Seq("Province" -> "Shanghai", "District" -> "Minhang", "mixin" -> "Building"),
+      "Building" -> Seq("BuildingName" -> "TangChenYiPin", "Height" -> "300M")
+    )
+    assert(expand(defs) == Seq("User" -> Seq("name" -> "oldpig", "gender" -> "male", "Province" -> "Shanghai", "District" -> "Minhang", "BuildingName" -> "TangChenYiPin", "Height" -> "300M"),
+      "Address" -> Seq("Province" -> "Shanghai", "District" -> "Minhang", "BuildingName" -> "TangChenYiPin", "Height" -> "300M"), "Building" -> Seq("BuildingName" -> "TangChenYiPin", "Height" -> "300M")))
+  }
 
 }
